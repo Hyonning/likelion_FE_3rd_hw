0d8991203e0746e0e0098d52785789069fc3ec91
2021110765@dgu.ac.kr <2021110765@dgu.ac.kr>
2024-06-18 00:13:55 +0900

    add review&comment

--- project/static/js/main/product_detail_page.js
+++ project/static/js/main/product_detail_page.js
@@ -1,1 +1,13 @@
 /* product detail page - JS */
+
+// const textarea = document.getElementById("comment-textarea");
+// const element = getElementById.content;
+
+// eventTarget.addEventListener("input", (event) => {
+
+
+//     if(textarea.value)
+
+//     element.disabled = false; 
+//     element.disabled = true; 
+//   });
